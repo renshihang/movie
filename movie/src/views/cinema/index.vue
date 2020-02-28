be8069e264c0cc075abed33ee083f07aac5bf060
renshihang <renshihang>
2020-02-28 20:11:27 +0800

    add createComponents

---
+++ movie/src/views/cinema/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<div id="main">
+		<div id="content">
+		<Header title="附近影院"/>
+			<div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+			<Cilist />
+		<Tabbar/>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+import Header from'@/components/Header/index.vue';
+import Tabbar from'@/components/Tabbar/index.vue';
+import Cilist from'@/components/Cilist/index.vue';
+
+export default{
+		name:'cinema',
+		components:{
+			Header,
+			Tabbar,
+			Cilist,
+
+			
+		},
+		watch:{
+			'$route'(cinema,movie){
+				console.log(cinema);
+				console.log(movie);
+				
+
+			}
+		}
+
+	}
+
+</script>
+
+<style scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+
+
+
+</style>
---
+++ movie/src/views/mine/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="main">
+		
+		<Header title="我的"/>
+		<div id="content">
+			<Login />
+		</div>
+
+		<Tabbar/>
+		
+	</div>
+</template>
+
+<script type="text/javascript">
+import Header from'@/components/Header/index.vue';
+import Tabbar from'@/components/Tabbar/index.vue';
+import Login from'@/components/Login/index.vue';
+
+export default{
+		name:'mine',
+		components:{
+			Header,
+			Tabbar,
+			Login
+
+			
+		}
+}
+</script>
+
+<style>
+
+</style>
